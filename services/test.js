c5b04b3549f67a6097e5934ac4dc9d3bb6f9ab74
Mr_Pan <panguoxian@users.noreply.github.com>
2023-01-14 18:39:56 +0800

    首次提交
    测试项目，VUE，引用同一个js,并同时调用方法，run后小程序不显示。

---
+++ services/test.js
@@ -0,0 +1,12 @@
+
+const open= () => {
+    console.log('open')
+}
+const close= () => {
+    console.log('close')
+}
+
+export default {
+    open,
+    close
+}
---
+++ src/pages/testpage/testpage.vue
@@ -0,0 +1,34 @@
+<template>
+  <view class="testpage">
+    <button @click="testOn">button</button>
+  </view>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue';
+import bt from '../../../services/test';
+
+
+export default {
+  name: 'Index',
+  components: {
+    
+  },
+  setup() {
+    
+    const testOn = ()=>{
+      bt.close();
+    }
+
+    return {
+      ...toRefs(state),
+      handleClick,
+      testOn
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
